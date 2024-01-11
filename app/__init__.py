dab0b6fe5a64343274d7693fabbb46aaa7ce6a8a
rttaso <anastasia.narimanashvilite01@geolab.edu.ge>
2024-01-11 04:35:53 +0400

    appupdate

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 from extensions import app
-from routes import home, list_films, error, rate_film, edit_film, delete_film, search, register, signin, logout, profile
+from routes import home, list_films, error, rate_film, edit_film, delete_film, search, register, signin, logout, profile, view_film, add_comment
